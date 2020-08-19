9405c2d4a00765c688cba0f87e36d4f81deffb0e
Arkam Mazrui <superarkam@gmail.com>
2020-08-19 17:15:02 -0400

    CREATE USER TASK FUNCTIONAL COMMIT

--- formCreateCI.js
+++ formCreateCI.js
@@ -21,6 +21,7 @@ var formCreateCI = {
         {
             formLocation: "/CustomSpace/Templates/createUser/createUser.html",
             formJSON: "/CustomSpace/Templates/createUser/createUser.json",
+            name: "createUser",
             displayName: "External User",
             classId: "10a7f898-e672-ccf3-8881-360bfb6a8f9a",
             icon: "fa-user"
@@ -29,10 +30,10 @@ var formCreateCI = {
 
     setup: [
         function() {
-            formCreateCI.classes.forEach(function(n,i){
+            formCreateCI.classes.forEach(function(ciClass,i){
                 formCreateCI.templates[menu2] += `<li class="drawer-button config-item-drawer1" 
                 data-click="open" data-desc="` + ciClass.displayName + `" data-click-template="`
-                 + ciClass.classId + `"><i class="drawer-icon fa fa-` + ciClass.icon + `"></i>
+                 + ciClass.classId + `"><i class="drawer-icon fa ` + ciClass.icon + `"></i>
                 <span class="drawermenu-tile-link">` + ciClass.displayName + `</span></li>`;
             });
 
@@ -72,7 +73,7 @@ var formCreateCI = {
                 },function (){}
             ).click(function () {
                 $('.drawermenu-menu > .drawermenu-tile[data-level="1"]').remove();
-                $('.drawermenu-tile[data-level="0"]').after(menu2);
+                $('.drawermenu-tile[data-level="0"]').after(formCreateCI.templates[menu2]);
                 
                 $('.drawermenu-tile[data-level="0"] > ul > li').removeClass('drawermenu-selected');
                 $('.config-item-drawer0').addClass('drawermenu-selected');
@@ -86,6 +87,10 @@ var formCreateCI = {
                     formCreateCI.functionality.showClassFormHTML(this_class_id);
                     $('.drawermenu-tile[data-level="1"] > ul > li').removeClass('drawermenu-selected');
                     $('.config-item-drawer1').addClass('drawermenu-selected');
+                    $(".drawermenu-createbutton").css("display", "block").click(function() {
+                        setTimeout(function(){}, 500);
+                        formCreateCI.functionality.commit_new_class(this_class_id);
+                    });
                 });
             });
         }
@@ -117,6 +122,67 @@ var formCreateCI = {
                     $(".drawerdetails-actions-box").html(result);
                 }
             });
+        },
+
+        get_input: function(name) {
+            return $(`input[name='`+name+`']`);
+        },
+
+        test_inputs: function(classId) {
+            let c = formCreateCI.functionality.getClassAtId(classId);
+            let requirement_met = true;
+            c.formJSON[c.name].fields.forEach(function(field,i){
+                let input = formCreateCI.functionality.get_input(field.name);
+                if (field.required && !input.val().length) {
+                    requirement_met = false;
+                    input.after(`<div class='alert alert-danger .alert-dismissable'>
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Required!</div>`);
+                }
+            });
+            return requirement_met;
+        },
+
+        get_commit_data: function(classId) {
+            var return_data = null;
+            var data = {ClassTypeId: classId};
+            let c = formCreateCI.functionality.getClassAtId(classId);
+
+            c.formJSON[c.name].fields.forEach(function(field,i){
+                data[field.name] = formCreateCI.functionality.get_input(field.name).val();
+            });
+
+            return_data = {
+                formJSON: {
+                    original: null,
+                    current: data
+                }
+            }
+
+            return return_data;
+        },
+
+        commit_new_class: function(classId) {
+            if (formCreateCI.functionality.test_inputs(classId)) {
+                kendo.confirm("Are you sure you want to create a new item?").then(function(){
+                    $.ajax({
+                        url: '/api/V3/Projection/Commit',
+                        type: 'post',
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        data: JSON.stringify(formCreateCI.functionality.get_commit_data(classId)),
+                        success: function(result) {
+                            kendo.alert(`<a href='/DynamicData/Edit/`+result.BaseId+`'>
+                            New configuration item successfully created!</a>`);
+                        },
+                        error: function(o, status, msg) {
+                            console.log("An error occured: " + status + ": " + msg);
+                            console.log(o.responseJSON.exception);
+                        }
+                    });
+                }, function() {
+                    kendo.alert("Cancelled creation of new configuration item.");
+                });
+            }
         }
     },
 
