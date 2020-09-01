affe8eb0b7331f248a7da2616e4b4aabf3db8af5
Arkam Mazrui <superarkam@gmail.com>
2020-09-01 17:02:04 -0400

    Fully functional commit!

--- templateApplier.js
+++ templateApplier.js
@@ -5,17 +5,12 @@
 var templateApplier = {
     properties: {
         template_applier_html: `<div id='template_applier'></div>`,
-        input_html: `<div id='template_input_container><input id='template_applier_select'/></div>'`,
+        input_html: `<div id='template_input_container'><input id='template_applier_select'/></div>'`,
         dialog: {
             width: "502px",
             title: "Apply Template",
             modal: true,
             visible: false,
-            content: templateApplier.properties.input_html,
-            actions: [
-                {text: "Apply", action: templateApplier.functionality.apply, primary: true},
-                {text: "Cancel", action: templateApplier.functionality.cancel, primary: false}
-            ]
         },
         comboBox: {
             dataTextField: "Name",
@@ -35,38 +30,151 @@ var templateApplier = {
             filter: "contains",
             suggest: true,
             index: 0
-        }
+        },
+        currentTicket: {
+            formObj: null,
+            viewModel: null
+        },
+        whitelist: ["Activity", "Area"]
     },
 
     getters: {
+        get_dialog: function() {
+            var r = templateApplier.properties.dialog;
+            r.content = templateApplier.properties.input_html;
+            r.actions = [
+                {text: "Apply", action: templateApplier.functionality.apply, primary: true},
+                {text: "Cancel", action: templateApplier.functionality.cancel, primary: false}
+            ];
+            return r;
+        },
+
         get_template_applier: function() {
             return $("#template_applier");
         },
 
+        get_dialog_window: function() {
+            return templateApplier.getters.get_template_applier().data("kendoDialog");
+        },
+
         get_input: function() {
             return $("#template_applier_select");
+        },
+
+        get_combobox: function() {
+            return templateApplier.getters.get_input().data("kendoComboBox");
+        },
+
+        get_selected_template_id: function() {
+            return templateApplier.getters.get_combobox().value();
+        },
+
+        get_whitelisted_properties: function() {
+            return templateApplier.properties.whitelist;
+        }
+    },
+
+    setters: {
+        set_ticket_info: function(formObj, viewModel) {
+            templateApplier.setters.set_formObj(formObj);
+            templateApplier.setters.set_viewModel(viewModel);
+        },
+
+        set_formObj: function(formObj) {
+            templateApplier.properties.currentTicket.formObj = formObj;
+        },
+
+        set_viewModel: function(viewModel) {
+            templateApplier.properties.currentTicket.viewModel = viewModel;
         }
     },
 
     functionality: {
         cancel: function() {return true},
-        apply: function() {console.log("Placeholder apply!"); return true}
+
+        apply: async function() {
+            templateApplier.getters.get_dialog_window().close();
+            var selected = templateApplier.getters.get_selected_template_id();
+            var templateObj = await templateApplier.functionality.request_template_obj(selected);
+            var current_obj = templateApplier.properties.currentTicket.viewModel;
+            if (templateObj.ClassTypeId !== current_obj.ClassTypeId) {
+                kendo.alert("Cannot apply template with class " + templateObj.ClassName + ` to 
+                object of type ` + current_obj.ClassName + '.');
+                return;
+            }
+            var whitelist = templateApplier.getters.get_whitelisted_properties();
+            var new_obj = templateApplier.functionality.replace_properties(current_obj, templateObj, whitelist);
+            templateApplier.functionality.commit_new_obj(new_obj, current_obj);
+
+        },
+
+        replace_properties: function(main_obj, replacement_obj, whitelist_properties) {
+            var r = $.extend([], main_obj);
+            if (!whitelist_properties || !Array.isArray(whitelist_properties)) {
+                whitelist_properties = [];
+            }
+
+            whitelist_properties.forEach(function(property) {
+                if (replacement_obj[property] === undefined) {
+                    delete r[property];
+                } else {
+                   r[property] = replacement_obj[property];
+                }
+            });
+
+            return r;
+        },
+
+        request_template_obj: async function(templateId) {
+            var req = {id: templateId, createdById: session.user.Id};
+            var url = window.location.origin + '/api/V3/Projection/GetProjectionByTemplateWithParameter';
+            var r = await ClientRequestManager.send_request("get", url, req, false);
+            return JSON.parse(r);
+        },
+
+        generate_commit_data: function(new_obj, old_obj) {
+          return {
+              formJSON: {
+                  original: old_obj,
+                  current: new_obj
+              }
+          };  
+        },
+
+        commit_new_obj: function(new_obj, old_obj) {
+            kendo.confirm("Are you sure you want to apply this template?").then(function(){
+                $.ajax({
+                    url: '/api/V3/Projection/Commit',
+                    type: 'post',
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+                    data: JSON.stringify(templateApplier.functionality.generate_commit_data(new_obj, old_obj)),
+                    success: function(result) {
+                        kendo.alert(`<a href='`+window.location.href+`'>
+                        Template successfully applied!</a>`);
+                    },
+                    error: function(o, status, msg) {
+                        console.log("An error occured: " + status + ": " + msg);
+                        console.log(o.responseJSON.exception);
+                    }
+                });
+            });
+        }
     },
 
     setup: [
         function() {
             //build UI
             $("body").append(templateApplier.properties.template_applier_html);
-            templateApplier.getters.get_template_applier().kendoDialog(templateApplier.properties.dialog);
+            templateApplier.getters.get_template_applier().kendoDialog(templateApplier.getters.get_dialog());
             templateApplier.getters.get_input().kendoComboBox(templateApplier.properties.comboBox);
         },
 
         function() {
             //create task
             app.custom.formTasks.add('ServiceRequest', 'Apply Template', function(formObj, viewModel){
-                console.log(formObj);
-                console.log(viewModel);
-                $("#my_modal").data("kendoDialog").open();
+                templateApplier.setters.set_ticket_info(formObj, viewModel);
+                templateApplier.getters.get_dialog_window().open();
             });
         }
     ],
