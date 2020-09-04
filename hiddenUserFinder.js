0c914e8567232dab1df8f47c6aadb5d8cf8fd02b
Arkam Mazrui <superarkam@gmail.com>
2020-09-04 13:44:06 -0400

    Fully functional userpickers commit

--- hiddenUserFinder.js
+++ hiddenUserFinder.js
@@ -9,9 +9,37 @@ var hiddenUserFinder = {
             return JSON.parse(JSON.parse(waiter.get_return()).value[0].EntityJson);
         },
 
+        get_users: function(name){
+            var r = [];
+            waiter.request("get", "/platform/api/GlobalSearch(EntitySets=@entitysets)?search="+name+"&%24top=10&%40entitysets=%27Cached_MT_System_Domain_User%27&languageFields=LanguageCode%2CLocaleID&languageCode=ENU&searchLimit=10?", {}, false);
+            var users = JSON.parse(waiter.get_return()).value;
+            users.forEach(function(user){
+                r.push(hiddenUserFinder.functionality.create_user_obj(JSON.parse(user.EntityJson)));
+            });
+            return r;
+        },
+
+        create_user_obj(user) {
+            return {Id: user.Guid, Name: user.DisplayName, Email: user.UPN};
+        },
+
         create_user: function(name) {
             var user = hiddenUserFinder.functionality.get_user(name);
-            var r = {Id: user.Guid, Name: user.DisplayName, Email: user.UPN};
+            return hiddenUserFinder.functionality.create_user_obj(user);
+        },
+
+        user_equals: function(u1, u2) {
+            return u1.Id == u2.Id;
+        },
+
+        user_array_includes(array, userobj) {
+            var r = false;
+            array.forEach(function(user){
+                if (hiddenUserFinder.functionality.user_equals(user, userobj)) {
+                    r = true;
+                    return;
+                }
+            });
             return r;
         }
     },
@@ -35,13 +63,16 @@ var hiddenUserFinder = {
                             //Set a timeout to allow the user to finish typing
                             timeout = setTimeout(function(){
                                 if (n.popup.visible()) {
-                                    if (n.dataSource._total == 0) {
-                                        var text = n.element.val();
-                                        try {
-                                            n.dataSource.add(hiddenUserFinder.functionality.create_user(text));
-                                        } catch(err) {
-                                            console.log("No such user " + text);
-                                        }
+                                    var text = n.element.val();
+                                    var displayed_users = n.dataSource.data();
+                                    var data_users = hiddenUserFinder.functionality.get_users(text);
+                                    if (data_users.length > displayed_users.length) {
+                                        data_users.forEach(function(user){
+                                            if (!hiddenUserFinder.functionality.user_array_includes(displayed_users, user)) {
+                                                n.dataSource.add(user);
+                                                displayed_users = n.dataSource.data();
+                                            }
+                                        });
                                     }
                                 }
                             }, accentSuggest.constants.type_speed_allowance);
