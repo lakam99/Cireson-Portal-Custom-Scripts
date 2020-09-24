30627366e08e4776394a9d493515673ff23f5d59
Arkam Mazrui <superarkam@gmail.com>
2020-09-24 12:53:56 -0400

    Added accent keyboard commit

---
+++ customSettings/accentKeyboard/accentKeyboard.js
@@ -0,0 +1,52 @@
+customSettings.helperFunctions.accentKeyboard = {};
+
+customSettings.helperFunctions.accentKeyboard.keys = [].concat.apply([], [
+    accentSuggest.constants.suggest_letters.a, accentSuggest.constants.suggest_letters.c,
+    accentSuggest.constants.suggest_letters.e, accentSuggest.constants.suggest_letters.i,
+    accentSuggest.constants.suggest_letters.o, accentSuggest.constants.suggest_letters.u,
+    accentSuggest.constants.suggest_letters.y]);
+
+customSettings.helperFunctions.accentKeyboard.build_UI = function(e) {
+    $(e).parent().before(`<div id='keyboard' class='container' style='border: 1px solid white " + 
+    "!important; background-color: #333333 !important'></div>`);
+    var keys = customSettings.helperFunctions.accentKeyboard.keys;
+    keys.forEach(function(letter){
+        $("#keyboard").append("<a class='k-btn btn btn-xs letter'>"+
+        String.fromCharCode(letter)+"</a>")
+    });
+    $(".letter").toArray().forEach(function(btn){
+        $(btn).on("click", function(){
+            var $input = $(btn).parent().parent().find('input');
+            var val = $input.val();
+            $input.val(val + $(btn).text());
+            setTimeout(function(){},1);
+        });
+    });
+}
+
+var accentKeyboardOn = function() {
+    var zxi = setInterval(function(){
+        var pickers = accentSuggest.getters.get_page_userpickers();
+        if (pickers.length) {
+            clearInterval(zxi);
+            pickers.forEach(function(e){
+                $(e).on("click", function(){
+                    if (!$("#keyboard").length) {
+                        customSettings.helperFunctions.accentKeyboard.build_UI(e);
+                        var parent = $(e).parent().parent().parent()[0];
+                        $(body).on("click", function(ev){
+                            if (!parent.contains(ev.target)) {
+                                $("#keyboard").remove();
+                                $(body).off("click");
+                            }
+                        });
+                    }
+                });
+            });
+        }
+    }, 100);
+}
+
+var accentKeyboardOff = function() {
+}
+
