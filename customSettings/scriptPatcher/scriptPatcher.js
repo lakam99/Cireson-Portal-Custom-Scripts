19ab640015576e746bdd3cba1e3340d6309b8ca7
Arkam Mazrui <superarkam@gmail.com>
2020-12-02 14:24:29 -0500

    Functional auto-update

---
+++ customSettings/scriptPatcher/scriptPatcher.js
@@ -0,0 +1,19 @@
+var scriptPatcherOn = function() {
+    $.ajax({
+        url: window.location.origin + "/CustomSpace/customSettings/scriptPatcher/versions.json?v="+Math.round(Math.random()*15),
+        dataType: "json",
+        success: function(res) {
+            var settings = "scriptPatcher-data";
+            var current = settings_controller.get_setting(settings);
+
+            if ((!current.data) || res.data.version > current.data.version) {
+                settings_controller.set_setting_value("update_required", true);
+            }
+
+            settings_controller.set_setting(settings, res);
+        }
+    });
+}
+
+var scriptPatcherOff = function() {
+}
