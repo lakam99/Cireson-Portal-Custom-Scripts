32c8d2b44e5d11f6b03748c42b7948050d2bbaaa
Arkam Mazrui <superarkam@gmail.com>
2020-12-03 15:41:47 -0500

    status patch

--- ticketConverter.js
+++ ticketConverter.js
@@ -103,7 +103,7 @@ var ticketConverter = {
             });
 
             ticketManipulator.set_obj_status(new_obj,
-                 (new_obj.FullClassName == "Incident" ? ticketManipulator.constants.statuses.completed.inc:ticketManipulator.constants.statuses.srq));
+                 (new_obj.FullClassName == "Incident" ? ticketManipulator.constants.statuses.completed.inc:ticketManipulator.constants.statuses.completed.srq));
             ticketManipulator.remove_loading();
             ticketConverter.functionality.ui_commit(old_obj, new_obj, convert_obj, type);
         },
